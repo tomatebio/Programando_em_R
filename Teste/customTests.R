456adc4b69129df9de8932303de6f33415f4fea4
tomatebio <carlostomate@gmail.com>
2018-07-07 01:04:46 -0300

    mudando gs key 2

--- Teste/customTests.R
+++ Teste/customTests.R
@@ -30,7 +30,7 @@ submit_log <- function(){
   library(googlesheets)
   suppressMessages(library(dplyr))
   # Please edit the link below
-  answer<-gs_key("1bkrQ8_-2kbR4-Hrk5t0nWOeX1tzC6Nayxkmk_jfVL0Q",lookup = FALSE, visibility = "private")
+  answer<-gs_key("1bkrQ8_-2kbR4-Hrk5t0nWOeX1tzC6Nayxkmk_jfVL0Q",lookup = FALSE)
   
   # Do not edit the code below
   
