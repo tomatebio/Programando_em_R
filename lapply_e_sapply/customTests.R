d109201d04cfa22d499040b54094c16c7dda9546
tomatebio <carlostomate@gmail.com>
2019-04-11 22:19:12 -0300

    add lapply_esapply

---
+++ lapply_e_sapply/customTests.R
@@ -0,0 +1,93 @@
+# Put custom tests in this file.
+      
+# Uncommenting the following line of code will disable
+# auto-detection of new variables and thus prevent swirl from
+# executing every command twice, which can slow things down.
+      
+#AUTO_DETECT_NEWVAR <- FALSE
+      
+# However, this means that you should detect user-created
+# variables when appropriate. The answer test, creates_new_var()
+# can be used for for the purpose, but it also re-evaluates the
+# expression which the user entered, so care must be taken.
+
+ # load library
+
+
+# Get the swirl state
+getState <- function(){
+  # Whenever swirl is running, its callback is at the top of its call stack.
+  # Swirl's state, named e, is stored in the environment of the callback.
+  environment(sys.function(1))$e
+}
+
+# Retrieve the log from swirl's state
+getLog <- function(){
+  getState()$log
+}
+
+
+
+submit_log <- function(){
+     yn <- select.list(c("Sim","Não"), graphics=FALSE)
+     if(yn=="Não") {
+        cat("Não será enviado")
+        return(TRUE)  }
+
+  cat("Preparando o envio ...\n")
+  library(googlesheets)
+  suppressMessages(library(dplyr))
+  # Please edit the link below
+  answer<-gs_key("1bkrQ8_-2kbR4-Hrk5t0nWOeX1tzC6Nayxkmk_jfVL0Q",  lookup = FALSE, visibility = "private")
+  
+  # Do not edit the code below
+  
+  p <- function(x, p, f, l = length(x)){if(l < p){x <- c(x, rep(f, p - l))};x}
+  
+  temp <- tempfile()
+  log_ <- getLog()
+  nrow_ <- max(unlist(lapply(log_, length)))
+  log_tbl <- data.frame(user = rep(log_$user, nrow_),
+                        course_name = rep(log_$course_name, nrow_),
+                        lesson_name = rep(log_$lesson_name, nrow_),
+                        question_number = p(log_$question_number, nrow_, NA),
+                        correct = p(log_$correct, nrow_, NA),
+                        attempt = p(log_$attempt, nrow_, NA),
+                        skipped = p(log_$skipped, nrow_, NA),
+                        datetime = p(log_$datetime, nrow_, NA),
+                        stringsAsFactors = FALSE)
+  write.csv(log_tbl, file = temp, row.names = FALSE)
+  encoded_log <- base64encode(temp)
+    #browseURL(paste0(pre_fill_link, encoded_log))
+
+  #  answer<- answer %>%
+  gs_add_row(answer,input=data.frame(Sys.time(),encoded_log))
+   return(TRUE)
+    
+}
+
+
+
+# Returns TRUE if the user has calculated a value equal to that calculated by the given expression.
+calculates_same_value <- function(expr){
+  e <- get("e", parent.frame())
+  # Calculate what the user should have done.
+  eSnap <- cleanEnv(e$snapshot)
+  val <- eval(parse(text=expr), eSnap)
+  passed <- isTRUE(all.equal(val, e$val))
+  if(!passed)e$delta <- list()
+  return(passed)
+}
+
+
+readline_clean <- function(prompt = "") {
+  wrapped <- strwrap(prompt, width = getOption("width") - 2)
+  mes <- stringr::str_c("| ", wrapped, collapse = "\n")
+  message(mes)
+  readline()
+}
+
+hrule <- function() {
+  message("\n", paste0(rep("#", getOption("width") - 2), collapse = ""), "\n")
+}
+
