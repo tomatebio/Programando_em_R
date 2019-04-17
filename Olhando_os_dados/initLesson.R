c91785ecad43bdfd9dfc260e649a33cb33c3c936
tomatebio <carlostomate@gmail.com>
2019-04-17 14:07:10 -0300

    arquivo iniciacao

--- Olhando_os_dados/initLesson.R
+++ Olhando_os_dados/initLesson.R
@@ -4,3 +4,20 @@
 # throughout the lesson.
 
 swirl_options(swirl_logging = TRUE)
+
+# Path to data
+.datapath <- file.path(path.package('swirl'), 'Courses',
+                      'Programando_em_R', 'Olhando_os_dados',
+                      'plant-data.txt')
+# Read in data
+plants <- read.csv(.datapath, strip.white=TRUE, na.strings="")
+
+# Remove annoying columns
+.cols2rm <- c('Accepted.Symbol', 'Synonym.Symbol')
+plants <- plants[, !(names(plants) %in% .cols2rm)]
+
+# Make names pretty
+names(plants) <- c('Scientific_Name', 'Duration', 'Active_Growth_Period',
+                   'Foliage_Color', 'pH_Min', 'pH_Max',
+                   'Precip_Min', 'Precip_Max',
+                   'Shade_Tolerance', 'Temp_Min_F')
