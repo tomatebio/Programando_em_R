16b2dbc23880bb0a6a6c004e336f6f3c3a0231d8
tomatebio <carlostomate@gmail.com>
2021-05-02 05:39:37 -0300

    correção de funcao

--- Matrizes_e_data_Frames/customTests.R
+++ Matrizes_e_data_Frames/customTests.R
@@ -9,6 +9,19 @@
    return(passed)
  }
 
+ # Retrieve the log from swirl's state
+
+ # Get the swirl state
+ getState <- function(){
+   # Whenever swirl is running, its callback is at the top of its call stack.
+   # Swirl's state, named e, is stored in the environment of the callback.
+   environment(sys.function(1))$e
+ }
+ 
+ getLog <- function(){
+   getState()$log
+ }
+
 
  submit_log <- function(){
       yn <- select.list(c("Sim","Não"), graphics=FALSE)
